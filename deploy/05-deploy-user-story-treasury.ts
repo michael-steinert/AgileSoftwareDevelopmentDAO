3ffd0b6cbec530b78f3f8556f6403cafc0af4f85
Michael Steinert <Michael.Steinert.94@gmail.com>
2022-05-29 16:58:08 +0200

    Improve Script

--- deploy/05-deploy-user-story-treasury.ts
+++ deploy/05-deploy-user-story-treasury.ts
@@ -2,32 +2,32 @@ import {HardhatRuntimeEnvironment} from "hardhat/types";
 import {DeployFunction} from "hardhat-deploy/types";
 import verify from "../utils/verify";
 import {networkConfig, developmentChains} from "../utils/hardhat-config";
-import {ethers} from "hardhat";
+import {deployments, ethers} from "hardhat";
+import {UserStoryTreasury} from "../typechain-types";
 
 const deployUserStoryTreasury: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
     const {getNamedAccounts, deployments, network} = hre;
     const {deploy, log} = deployments;
     const {deployer} = await getNamedAccounts();
-    log("----------------------------------------------------");
     log("Deploying User Story Treasury and waiting for Confirmations");
-    /* Deployment Object `UserStory` that can not invoke Function from Contract `Box` */
-    const userStory = await deploy("UserStoryTreasury", {
+    /* Deployment of `UserStoryTreasury` */
+    await deploy("UserStoryTreasury", {
         from: deployer,
         args: [],
         log: true,
         /* Waiting some Block Confirmation, so on a Testnet or Mainnet it can be verified properly */
         waitConfirmations: networkConfig[network.name].blockConfirmations || 1
-    })
-    log(`UserStory at ${userStory.address}`);
+    });
+    const UserStoryTreasury = await deployments.get("UserStoryTreasury");
+    const userStoryTreasury = (await ethers.getContractAt("UserStoryTreasury", UserStoryTreasury.address)) as UserStoryTreasury;
+    log(`UserStoryTreasury at ${userStoryTreasury.address}`);
     if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
-        await verify(userStory.address, []);
+        await verify(userStoryTreasury.address, []);
     }
     /* Contract Object `userStory` that can invoke Function from Contract `UserStoryTreasury` */
-    const userStoryTreasury = await ethers.getContractAt("UserStoryTreasury", userStory.address);
     const TimeLockController = await deployments.get("TimeLock");
-    const timeLockController = await ethers.getContractAt("TimeLock", TimeLockController.address);
     /* Giving Ownership of Contract `UserStoryContract` from `deployer` to Contract `TimeLockController` */
-    const transferTransaction = await userStoryTreasury.transferOwnership(timeLockController.address);
+    const transferTransaction = await userStoryTreasury.transferOwnership(TimeLockController.address);
     await transferTransaction.wait();
 }
 
