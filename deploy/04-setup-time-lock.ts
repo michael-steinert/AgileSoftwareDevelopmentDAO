eb7366bd6c876df959dd3213af2772156b2abf66
Michael Steinert <Michael.Steinert.94@gmail.com>
2022-10-14 09:23:14 +0200

    Improved Comments

--- deploy/04-setup-time-lock.ts
+++ deploy/04-setup-time-lock.ts
@@ -19,35 +19,41 @@ const setupDaoGovernor: DeployFunction = async (
     DaoGovernor.address
   );
   log('----------------------------------------------------');
-  /* A Role can only be granted or revoked by someone with the Admin Role */
-  /* Upon Deployment of the Time Lock Controller, both the Time Lock Controller and the Deployer of the Smart Contract have this Privilege */
+  /* 
+  A Role can only be granted or revoked by someone with the Admin Role
+  Upon Deployment of the Time Lock Controller, both the Time Lock Controller and the Deployer of the Smart Contract have this Privilege
+  */
   log('Setting up Time Lock Controller with new Roles');
-  /* Proposer is an Address (Smart Contract or EOA) responsible for Scheduling (and Cancelling) Operations */
+  // Proposer is an Address (Smart Contract or EOA) responsible for Scheduling (and Cancelling) Operations
   const proposerRole = await timeLockController.PROPOSER_ROLE();
-  /* Executor is an Address (Smart Contract or EOA) responsible for Executing Operations once the Time Lock has expired */
+  // Executor is an Address (Smart Contract or EOA) responsible for Executing Operations once the Time Lock has expired
   const executorRole = await timeLockController.EXECUTOR_ROLE();
-  /* Admin is an Address (Smart Contract or EOA) that is in Charge of Granting the Roles of Proposer and Executor */
+  // Admin is an Address (Smart Contract or EOA) that is in Charge of Granting the Roles of Proposer and Executor
   const adminRole = await timeLockController.TIMELOCK_ADMIN_ROLE();
-  /* `deployer` owns the Contract `timeLockContract` so he can change the Roles */
-  /* Only Contract `timeLockController` can create Proposals */
+  /*
+  `deployer` owns the Contract `timeLockContract` so he can change the Roles
+  Only Contract `timeLockController` can create Proposals
+  */
   const proposerTransaction = await timeLockController.grantRole(
     proposerRole,
     daoGovernor.address
   );
   await proposerTransaction.wait();
-  /* Everyone can execute a Proposal after the TimeLock Delay */
+  // Everyone can execute a Proposal after the TimeLock Delay
   const executorTransaction = await timeLockController.grantRole(
     executorRole,
     ethers.constants.AddressZero
   );
   await executorTransaction.wait();
-  /* `deployer` does not own the Contract `timeLockContract` anymore, so he can not change the Roles */
+  // `deployer` does not own the Contract `timeLockContract` anymore, so he can not change the Roles
   const revokeTransaction = await timeLockController.revokeRole(
     adminRole,
     deployer
   );
-  /* Now, every Action that the Contract TimeLock wants to do has to go through the Governance Process */
-  /* Giving Users this Level of Control will lead to unique and imaginative Governance Use Cases */
+  /*
+  Now, every Action that the Contract TimeLock wants to do has to go through the Governance Process
+  Giving Users this Level of Control will lead to unique and imaginative Governance Use Cases
+  */
   await revokeTransaction.wait();
 };
 
